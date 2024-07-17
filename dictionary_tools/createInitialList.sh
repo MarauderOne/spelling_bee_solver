732973566921b5de0988b9744c7ff075f57ae48b
Alexander Berridge <alexjberridge@gmail.com>
2024-07-17 09:33:45 +0100

    Adding dictionary tools and initial dictionary

---
+++ dictionary_tools/createInitialList.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -f my.dict
+rm -f initialList.dict
+#sudo apt install aspell
+aspell -d en dump master | aspell -l en expand >my.dict
+grep -v "'" my.dict | grep -v "^.$\|^..$\|^...$" | grep -v '[^[:lower:]]' | tr '[:lower:]' '[:upper:]' | sort | uniq | grep -v "^HDQRS$" >>initialList.dict
+rm -f my.dict
---
+++ dictionary_tools/dictionary_tools.go
@@ -0,0 +1,82 @@
+package dictionary_tools
+
+import (
+	"github.com/ddliu/go-dict/util"
+	"regexp"
+)
+
+func NewSimpleDict() *MySimpleDict {
+	return &MySimpleDict{}
+}
+
+type MySimpleDict struct {
+	Words []string
+}
+
+func (this *MySimpleDict) AddWordsList(words []string) {
+	for _, w := range words {
+		this.Words = append(this.Words, w)
+	}
+}
+
+func (this *MySimpleDict) Count() int {
+	return len(this.Words)
+}
+
+func (this *MySimpleDict) Load(dict string) {
+	util.WalkFileLines(dict, func(line string) bool {
+		this.Words = append(this.Words, string(line[:]))
+		return true
+	})
+}
+
+func (this *MySimpleDict) Lookup(pattern string, offset int, limit int) []string {
+	if pattern == "" {
+		if offset == 0 && limit == 0 {
+			return this.Words[:]
+		}
+		if offset >= len(this.Words) {
+			return nil
+		}
+
+		var end int
+		if limit <= 0 {
+			end = len(this.Words)
+		} else {
+			end = offset + limit
+		}
+
+		return this.Words[offset:end]
+	}
+
+	var compiledPattern = regexp.MustCompile("^" + pattern + "$")
+	var matched []string
+	found := 0
+	this.Walk(func(word string) bool {
+		if compiledPattern.MatchString(word) {
+			found++
+
+			if found < offset+1 {
+				return true
+			}
+
+			matched = append(matched, word)
+
+			if limit > 0 && found >= offset+limit {
+				return false
+			}
+		}
+
+		return true
+	})
+
+	return matched
+}
+
+func (this *MySimpleDict) Walk(f func(string) bool) {
+	for i := 0; i < len(this.Words); i++ {
+		if !f(this.Words[i]) {
+			break
+		}
+	}
+}
